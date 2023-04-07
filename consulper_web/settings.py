74bff94ca35dabe33252f9ee61d7564181e33cfd
ShifterNathan <78045061+ShifterNathan@users.noreply.github.com>
2023-04-07 15:34:37 -0300

    Update settings.py

--- consulper_web/settings.py
+++ consulper_web/settings.py
@@ -134,5 +134,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "alternplayer00@gmail.com"
-EMAIL_HOST_PASSWORD = 'AlPl0192'
+EMAIL_HOST_PASSWORD = #Type the email password
 
